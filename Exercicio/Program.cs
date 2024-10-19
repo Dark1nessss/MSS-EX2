0987119bd4ccff0a72ff25a8f39dfe34c0d4abf2
Dark1nessss <frankhorn99@gmail.com>
2024-10-19 12:50:37 +0100

    Update Exercicio 1

--- Exercicio/Program.cs
+++ Exercicio/Program.cs
@@ -1,20 +1,22 @@
 ﻿using System;
 
-public class Invoice{
-
+public class Invoice
+{
     private string numeroItem;
     private string descricao;
     private int quantidade;
     private double precoUnidade;
 
-    public Invoice(string numeroItem, string descricao, int quantidade, double precoUnidade){
+    public Invoice(string numeroItem, string descricao, int quantidade, double precoUnidade)
+    {
         this.numeroItem = numeroItem;
         this.descricao = descricao;
         this.quantidade = quantidade > 0 ? quantidade : 0;
         this.precoUnidade = precoUnidade > 0 ? precoUnidade : 0.0;
     }
 
-    public string GetNumeroItem(){
+    public string GetNumeroItem()
+    {
         return numeroItem;
     }
 
@@ -22,7 +24,6 @@ public class Invoice{
     {
         this.numeroItem = numeroItem;
     }
-
 
     public string GetDescricao()
     {
@@ -34,7 +35,6 @@ public class Invoice{
         this.descricao = descricao;
     }
 
-
     public int GetQuantidade()
     {
         return quantidade;
@@ -44,7 +44,6 @@ public class Invoice{
     {
         this.quantidade = quantidade > 0 ? quantidade : 0;
     }
-
 
     public double GetPrecoUnitario()
     {
@@ -60,36 +59,52 @@ public class Invoice{
     {
         return quantidade * precoUnidade;
     }
-
 }
 
 class Program
 {
     static void Main(string[] args)
     {
-        Invoice invoice1 = new Invoice("001", "Teclado Mecânico", 5, 250.0);
+        Console.Write("Digite o número do item: ");
+        string numeroItem = Console.ReadLine();
 
+        Console.Write("Digite a descrição do item: ");
+        string descricao = Console.ReadLine();
+
+        Console.Write("Digite a quantidade comprada: ");
+        int quantidade = int.Parse(Console.ReadLine());
+
+        Console.Write("Digite o preço unitário: ");
+        double precoUnidade = double.Parse(Console.ReadLine());
+
+        Invoice invoice1 = new Invoice(numeroItem, descricao, quantidade, precoUnidade);
+
+        Console.WriteLine("\n--- Fatura ---");
         Console.WriteLine("Número do Item: " + invoice1.GetNumeroItem());
         Console.WriteLine("Descrição: " + invoice1.GetDescricao());
         Console.WriteLine("Quantidade: " + invoice1.GetQuantidade());
         Console.WriteLine("Preço Unitário: € " + invoice1.GetPrecoUnitario());
         Console.WriteLine("Valor da Fatura: € " + invoice1.GetInvoiceAmount());
 
-        invoice1.SetQuantidade(3);
-        invoice1.SetPrecoUnitario(200.0);
+        Console.WriteLine("\nAtualizando valores...");
+        Console.Write("Digite a nova quantidade: ");
+        invoice1.SetQuantidade(int.Parse(Console.ReadLine()));
+
+        Console.Write("Digite o novo preço unitário: ");
+        invoice1.SetPrecoUnitario(double.Parse(Console.ReadLine()));
 
         Console.WriteLine("\n--- Após Atualizações ---");
         Console.WriteLine("Quantidade Atualizada: " + invoice1.GetQuantidade());
         Console.WriteLine("Preço Unitário Atualizado: € " + invoice1.GetPrecoUnitario());
         Console.WriteLine("Novo Valor da Fatura: € " + invoice1.GetInvoiceAmount());
 
-
+        Console.WriteLine("\nTentando definir valores inválidos...");
         invoice1.SetQuantidade(-10);
         invoice1.SetPrecoUnitario(-50.0);
 
         Console.WriteLine("\n--- Após Tentativa de Valores Inválidos ---");
         Console.WriteLine("Quantidade: " + invoice1.GetQuantidade()); // 0
-        Console.WriteLine("Preço Unitário:  € " + invoice1.GetPrecoUnitario()); // 0.0
+        Console.WriteLine("Preço Unitário: € " + invoice1.GetPrecoUnitario()); // 0.0
         Console.WriteLine("Valor da Fatura: € " + invoice1.GetInvoiceAmount()); // 0.0
     }
 }
--- Exercicio/obj/Debug/net8.0/Exercicio.AssemblyInfo.cs
+++ Exercicio/obj/Debug/net8.0/Exercicio.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Exercicio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+07acacf36cf3d58f6d01922ef849bad7f1d4f7ca")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a063c0f5e23aee2140a2b363b0f25997835fb5c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Exercicio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Exercicio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
